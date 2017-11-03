7c3a31dd1c56cb9c6ed58cb2a83cf9c693278665
Marshall Houston <marshall.houston@gmail.com>
2017-11-03 10:36:15 -0600

    add average price for machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,4 +2,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :snack_locations
   has_many :snacks, through: :snack_locations
+
+  def average_price
+    snacks.average(:price)
+  end
 end
